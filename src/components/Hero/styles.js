74857c4d58532f037e2c895a9b42d7180afaaa50
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-09-17 19:15:45 -0300

    Começando Media desktop

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -7,11 +7,26 @@ export const Section = styled.section`
   background-position: 0 -40px;
   width: 100vw;
   height: 320px;
+  @media(min-width: 900px) {
+    background-image: url('/easybank-landing-page-master/images/bg-intro-desktop.svg');
+    height: 80vh;
+    background-position: 42vw -16vw;
+    background-repeat: no-repeat;
+    background-size: 70vw;
+    display: flex;
+    justify-content: flex-end;
+  }
 `;
 
 export const Img = styled.img`
-  width: 100vw;
+  width: 100%;
   height: 100%;
   object-fit: cover;
   object-position: 0 95%;
+  @media(min-width: 900px) {
+    width: 45vw;
+    margin: auto;
+    object-position: 7vw -10vw;
+    position: absolute;
+  }
 `;
--- src/components/IconsHeader/styles.js
+++ src/components/IconsHeader/styles.js
@@ -1,12 +1,42 @@
 import styled from 'styled-components';
 
-const Div = styled.div`
+export const Div = styled.div`
   width: 90vw;
   height: 60px;
   margin: 0 5vw;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  @media(min-width: 900px) {
+    width: 80vw;
+    margin: 0 10vw;
+  justify-content: space-between;
+
+  }
 `;
 
-export default Div;
+export const A = styled.a`
+  color: black;
+  text-decoration: none;
+  margin-right: 25px;
+  color: hsl(233, 8%, 62%);
+  font-weight: 500;
+`;
+
+export const Button = styled.button`
+  background-color: hsl(136, 65%, 51%);
+  background: 
+    linear-gradient( 90deg,
+            hsl(136, 65%, 51%), 
+            hsl(192, 70%, 51%)
+          );
+  color: white;
+  border-radius: 100px;
+  padding: 10px 30px;
+  font-weight: 900;
+
+`;
+
+export const Nav = styled.nav`
+
+`;
